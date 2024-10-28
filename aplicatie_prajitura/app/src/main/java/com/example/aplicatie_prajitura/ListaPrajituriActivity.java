3950a855ee5a755917f6963b4800380ddf7177f4
cristinaandreea18 <cristinaandreea577@gmail.com>
2024-10-28 19:26:10 +0200

    seminar 5

---
+++ aplicatie_prajitura/app/src/main/java/com/example/aplicatie_prajitura/ListaPrajituriActivity.java
@@ -0,0 +1,57 @@
+package com.example.aplicatie_prajitura;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.List;
+
+public class ListaPrajituriActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_lista_prajituri);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Intent it = getIntent();
+        List<Prajitura> prajituri = it.getParcelableArrayListExtra("listaPrajituri1");
+
+        ListView lv = findViewById(R.id.prajituriLV);
+
+        ArrayAdapter<Prajitura> adapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1,prajituri);
+        lv.setAdapter(adapter);
+
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(getApplicationContext(),prajituri.get(i).toString(),Toast.LENGTH_LONG).show();
+            }
+        });
+
+        lv.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                prajituri.remove(i);
+                adapter.notifyDataSetChanged();
+                return false;
+            }
+        });
+    }
+}
