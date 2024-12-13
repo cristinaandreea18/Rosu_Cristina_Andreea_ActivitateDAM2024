51b0d1fdd485caf7e0d82629771ade4745a7d284
cristinaandreea18 <140267118+cristinaandreea18@users.noreply.github.com>
2024-12-13 20:23:49 +0200

    Delete app5/app/src/main directory

--- app5/app/src/main/java/com/example/app5/CarteDatabase.java
+++
@@ -1,9 +0,0 @@
-package com.example.app5;
-
-import androidx.room.Database;
-import androidx.room.RoomDatabase;
-
-@Database(entities={Carte.class},version=1)
-public abstract class CarteDatabase extends RoomDatabase {
-    public abstract DAOCarte daoCarte();
-}
